0071177b12910d50a1d3fd2468f09ffbd63e9b20
carmenchuncita <63347617+carmenchuncita@users.noreply.github.com>
2020-04-22 19:01:43 +0200

    Add files via upload

---
+++ NVL-JS-P9b.js
@@ -0,0 +1,38 @@
+var Dias = new Array(2) 
+Dias[0] = "Lunes"
+Dias[1] = "Martes"
+Dias[2] = "Miércoles"
+Dias[3] = "Jueves"
+Dias[4] = "Viernes"
+Dias[5] = "Sábado"
+Dias[6] = "Domingo"
+
+var Ciudad = new Array (2) 
+Ciudad[0] = "Madrid"
+Ciudad[1] = "Sevilla"
+Ciudad[2] = "Barcelona"
+Ciudad[3] = "Valencia"
+Ciudad[4] = "Zaragoza"
+Ciudad[5] = "Bilbao"
+Ciudad[6] = "Donosti"
+
+var Resultado =new Array
+Resultado[0]=(Dias[0]+Ciudad[0]);
+Resultado[1]=(Dias[1]+Ciudad[1]);
+Resultado[2]=(Dias[2]+Ciudad[2]);
+Resultado[3]=(Dias[3]+Ciudad[3]);
+Resultado[4]=(Dias[4]+Ciudad[4]);
+Resultado[5]=(Dias[5]+Ciudad[5]);
+Resultado[6]=(Dias[6]+Ciudad[6]);
+
+
+
+console.log(Resultado)
+
+
+
+
+
+
+
+
