339b66b15ceb08da44bfcd7e7234118b306a2d62
carmenchuncita <63347617+carmenchuncita@users.noreply.github.com>
2020-04-22 12:54:48 +0200

    Add files via upload

---
+++ NVL-JS-P9a.js
@@ -0,0 +1,7 @@
+var menchu = [[1, 245], [3,4], [5,7], [8,6]]
+
+
+
+console.log(menchu.length, "----longitud del array!);")
+console.log(menchu[2].length, "----longitud del elemento 2!);")
+alert(menchu[3][1]);
